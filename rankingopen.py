2ba87566e2470b87704eb3212647cfcd63ff8ada
Grisha <oskin@phystech.edu>
2018-10-24 14:40:07 +0300

    initial commit

--- rankingopen.py
+++ rankingopen.py
@@ -9,18 +9,6 @@ from matplotlib.pyplot import figure, imshow, axis
 from matplotlib.image import imread
 import matplotlib.pyplot as plt
 
-def display_data(gallery,target,i):
-    ims = []
-    t_im = cv2.imread(target["path"])
-    cv2.rectangle(t_im,(0,0),(t_im.shape[0],t_im.shape[0]),(0,255,0),3)
-    ims.append(t_im)
-    
-    for e in gallery:
-        ims.append(cv2.imread(e["path"]))
-    numpy_horizontal = np.hstack(ims)
-    #cv2.imwrite("sameness_out/"+str(i)+".png", numpy_horizontal)
-    cv2.imshow('Main', numpy_horizontal)
-    cv2.waitKey(0)
 def get_distance(e1, e2):
     
     #print(np.array(globs).shape)
@@ -29,11 +17,14 @@ def get_distance(e1, e2):
     e1 = e1["features"][0].flatten()
     e2 = e2["features"][0].flatten()
     return np.linalg.norm(e1 - e2)
+    #return loc_dist[0][0] , glob_dist[0][0]
 
 Xf = open('data','rb')
 data = np.array(pickle.load(Xf))
 Xf.close()
+print("data size: ", len(data))
 
+print("feature shape: ", data[0]["features"][0].shape)
 same = {}
 for e in data:
     if e["id"] not in same:
@@ -42,16 +33,51 @@ for e in data:
    
 hits = 0
 ids = list(same.keys())
-n_steps = 10
+print("elements per id: ", len(data)/len(ids))
+
+###################################VARIABLES#######
+n_steps = 1000
+thresh = 0.85
+n_samples = 100
+####################################################
+
 hit_dist, miss_dist = [],[]
-for j in range(n_steps):
-    
+
+##############################################################
+for j in range(int(n_steps/2)):
     rand_init = []
     
     target_group = same[random.choice(ids)]
     target_id = random.randint(0,len(target_group)-1)
     target = target_group[target_id]
     #print(len(target_group))
+    gallery = []
+    new_data = []
+    for e in data:
+        if e["id"] != target["id"]:
+            new_data.append(e)
+    
+    
+    gallery.extend(random.sample(list(new_data), n_samples))
+    random.shuffle(gallery)
+    miss_d = [get_distance(v, target) for v in gallery[0:5]]
+    
+    gallery.sort(key=lambda e: get_distance(e, target))
+    sorted_miss_d = [get_distance(v, target) for v in gallery[0:5]]
+    ratio = np.mean(sorted_miss_d)/np.mean(miss_d)
+    miss_dist.append(ratio)
+    if ratio > thresh:
+        hits+=1
+    
+for j in range(int(n_steps/2)):
+    
+    rand_init = []
+    
+
+    target_group = same[random.choice(ids)]
+    target_id = random.randint(0,len(target_group)-1)
+    target = target_group[target_id]
+    
     gallery = []
     for i in range(len(target_group)):
         if i != target_id:
@@ -60,28 +86,24 @@ for j in range(n_steps):
     for e in data:
         if e["id"] != target["id"]:
             new_data.append(e)
-            
-    n_samples = min(len(new_data),1000)
+    
+    
     gallery.extend(random.sample(list(new_data), n_samples))
+    
+    random.shuffle(gallery)
+    
+    gal_dist = [get_distance(v, target) for v in gallery[0:5]]
     gallery.sort(key=lambda e: get_distance(e, target))
-    top5 = []
-    for i in range(5):
-        top5.append(gallery[i]["id"])
-    if top5[0] == target["id"]:# or get_distance(gallery[0], target) < 0.7:
-        hit_dist.append(get_distance(gallery[0], target))
+    sorted_gal_dist = [get_distance(v, target) for v in gallery[0:5]]
+    ratio = np.mean(sorted_gal_dist)/np.mean(gal_dist)
+    hit_dist.append(ratio)
+    if ratio < thresh:
         hits+=1
-    else:
-        miss_dist.append(get_distance(gallery[0], target))
-    display_data(gallery[0:10],target,j)   
-print("accuracy: ",hits/n_steps)
-print( "hit: ", np.mean(hit_dist))
-print("miss: ",np.mean(miss_dist))
+    
+print("accuracy: ", hits/n_steps)
 plt.hist(hit_dist, bins='auto')  # arguments are passed to np.histogram
 plt.hist(miss_dist, bins='auto')  # arguments are passed to np.histogram
 
 plt.title("Hits")
 plt.show()
 
-#plt.hist(miss_dist, bins='auto')  # arguments are passed to np.histogram
-#plt.title("Misses")
-#plt.show()
